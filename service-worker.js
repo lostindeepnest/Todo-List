c3cc885d5f964517784da049957b84abd589382b
Akshay <abhi.905.a@gmail.com>
2026-07-18 17:30:21 +0530

    sw update

--- service-worker.js
+++ service-worker.js
@@ -17,10 +17,31 @@ const FILES = [
 ];
 
 self.addEventListener("install", event => {
+    self.skipWaiting()
     event.waitUntil(
         caches.open(CACHE)
             .then(cache => cache.addAll(FILES))
     );
+});
+
+self.addEventListener("activate", event => {
+
+    event.waitUntil(
+        caches.keys().then(keys =>
+            Promise.all(
+                keys.map(key => {
+
+                    if (key !== CACHE) {
+                        return caches.delete(key);
+                    }
+
+                })
+            )
+        )
+    );
+
+    self.clients.claim();
+
 });
 
 self.addEventListener("fetch", event => {
